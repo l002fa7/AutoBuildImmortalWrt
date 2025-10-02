39a444eebd1d50bba9d7b1788036a8deeab97942
蘭 <129944338+l002fa7@users.noreply.github.com>
2025-10-02 18:37:21 +0800

    Update build24.sh

--- sunxi-cortexa53/build24.sh
+++ sunxi-cortexa53/build24.sh
@@ -36,9 +36,7 @@ echo "$(date '+%Y-%m-%d %H:%M:%S') - Starting build process..."
 # 定义所需安装的包列表 下列插件你都可以自行删减
 PACKAGES=""
 PACKAGES="$PACKAGES curl"
-PACKAGES="$PACKAGES luci-i18n-firewall-zh-cn"
 # 服务——FileBrowser 用户名admin 密码admin
-PACKAGES="$PACKAGES luci-i18n-filebrowser-go-zh-cn"
 PACKAGES="$PACKAGES luci-theme-argon"
 PACKAGES="$PACKAGES luci-app-argon-config"
 PACKAGES="$PACKAGES luci-i18n-argon-config-zh-cn"
